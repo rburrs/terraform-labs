c5b387d5f6199b0aec530e521d701ec7e37d94a4
rburrs <richard.burrs@gmail.com>
2021-08-18 11:14:00 -0400

    changed storage file

--- teststorage.tf
+++ teststorage.tf
@@ -1,13 +1,7 @@
-resource "azurerm_resource_group" "storerg" {
-    name = "storerg"
-    location = "${var.loc}"
-    tags     = "${var.tags}"
-}
-
 resource "azurerm_storage_account" "teststorage" {
     count = 3
     
-    name                     = "terraformteststor[count.index]"
+    name                     = "terraformteststor${count.index}"
     resource_group_name      = "${azurerm_resource_group.storerg.name}"
     location                 = "${azurerm_resource_group.storerg.location}"
     account_tier             = "Standard"
