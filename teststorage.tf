047a063020882b5e148b80d7b18e5e610286a07e
rburrs <richard.burrs@gmail.com>
2021-08-18 16:30:56 -0400

    deleted storage

--- teststorage.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_storage_account" "teststorage" {
-    count = 3
-    
-    name                     = "terraformteststor${count.index}"
-    resource_group_name      = "${azurerm_resource_group.storerg.name}"
-    location                 = "${azurerm_resource_group.storerg.location}"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
